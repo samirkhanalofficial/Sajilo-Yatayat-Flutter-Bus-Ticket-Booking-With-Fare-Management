55973d4a45cfccf081de865a81429a9691e578bc
Babita Shrestha <121298971+Babitashrestha340380@users.noreply.github.com>
2024-01-03 13:01:02 +0545

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,124 +1,258 @@
 import 'package:flutter/material.dart';
-import 'phone.dart';
 import 'package:lottie/lottie.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SAJILOYATAYAT(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SAJILOYATAYAT extends StatefulWidget {
+  const SAJILOYATAYAT({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Sajilo Yatayat',
-      theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const RoleSelectionPage(),
-    );
-  }
+  State<SAJILOYATAYAT> createState() => _SAJILOYATAYATState();
 }
 
-class RoleSelectionPage extends StatelessWidget {
-  const RoleSelectionPage({super.key});
+class _SAJILOYATAYATState extends State<SAJILOYATAYAT> {
+  final TextEditingController _pinController1 = TextEditingController();
+  final TextEditingController _pinController2 = TextEditingController();
+  final TextEditingController _pinController3 = TextEditingController();
+  final TextEditingController _pinController4 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-
-
-      body: Column(
-
-        children: [
-          Expanded(child:
-          Lottie.asset('assets/background_animation.json',
-            alignment: Alignment.topCenter,
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.fill,
-
-          ),
-          ),
-
-          const Text('Sajilo Yatayat',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
-          const Text('Everything you need for travel',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Choose your role:',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),)
-              ],
-            ),
-          ),
-
-
-          ElevatedButton(
-               style: ElevatedButton.styleFrom(
-                 primary: Colors.white,
-                shape:
-                RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10), // BorderRadius of the button
-                side: const BorderSide(color: Colors.green, width: 2), // Border color and width
-                   ),
-
-                 ),
-
-            onPressed: () {
-              // Navigate to the phone number entry page for passengers
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const PhoneNumberEntryPage(role: 'Passenger'),
-                ),
-              );
-            },
-            child: const Text('Passenger',style: TextStyle(color: Colors.green,fontSize: 20),textAlign: TextAlign.center,),
-          ),
-          const SizedBox(height: 20),
-
-
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              shape:
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10), // BorderRadius of the button
-                side: const BorderSide(color: Colors.green, width: 2), // Border color and width
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children:[
+            Align(
+              alignment: Alignment.topRight,
+              child: Image.asset(
+                'assets/circle.png',
+                height: 200, // Adjust the height as needed
               ),
-            )
+            ),
 
-            ,
-            onPressed: () {
-              // Navigate to the phone number entry page for bus owner
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const PhoneNumberEntryPage(role: 'Bus Owner'),
-                ),
-              );
-            },
-            child: const Text('Bus Owner',style: TextStyle(color: Colors.green,fontSize: 20),textAlign: TextAlign.center,),
+            Expanded(
+              child: Lottie.asset(
+                'assets/register.json',
+                alignment: Alignment.topCenter,
+                width: double.infinity,
+                height: 400, // Adjust the height as needed
+                fit: BoxFit.fill,
+              ),
+            )]),
+
+            SizedBox(height: 20),
+            Container(
+                padding: EdgeInsets.all(8),
+                child:Column(
+                    children:[
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("About You", style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold)),
+            ],
           ),
-
-
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Let us know more about you.", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 11)),
+            ],
+          ),
+            SizedBox(height: 15),
 
 
           Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-
-              Image.asset('assets/semi.png'),
+              Text("Full Name", style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold)),
             ],
           ),
-        ],
+            SizedBox(height: 10,),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Full Name',
+                labelText: 'Enter your full name',
+                border: OutlineInputBorder( borderRadius: BorderRadius.circular(10)),
+
+              ),
+            ),
+     SizedBox(height: 10,),
+     Column(
+       children:[
+       Row(
+         crossAxisAlignment: CrossAxisAlignment.start,
+         children: [
+           Text("Address", style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold,)),
+         ],
+       ),
+         SizedBox(height: 10,),
+         TextField(
+           decoration: InputDecoration(
+             hintText: 'Address',
+             labelText: 'Enter your address',
+             border: OutlineInputBorder( borderRadius: BorderRadius.circular(10)),
+           ),
+           ) ]),
+            SizedBox(height: 10,),
+
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Date of Birth", style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold)),
+              ],
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Date of Brith',
+                labelText: 'Enter your date of birth',
+                border: OutlineInputBorder( borderRadius: BorderRadius.circular(10)),
+              )),
+
+              SizedBox(height: 10,),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Transaction pin", style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold)),
+              ],
+            ),
+
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    
+                    controller: _pinController1,
+                    keyboardType: TextInputType.number,
+                    maxLength: 1,
+                    obscureText: false,
+                    textAlign: TextAlign.center,
+
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(10),
+                        counterText: '',
+                        border:OutlineInputBorder(borderRadius: BorderRadius.circular(20)
+                        ),
+                    ),
+                    onChanged: (String value) {
+                      if (value.length == 1) {
+                        FocusScope.of(context).nextFocus();
+                      }
+                    },
+                  ),
+                ),
+                SizedBox(width: 30),
+                Expanded(
+                  child: TextField(
+                    controller: _pinController2,
+                    keyboardType: TextInputType.number,
+                    maxLength: 1,
+                    obscureText: false,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(10),
+
+                      counterText: '',
+                      border:OutlineInputBorder(borderRadius: BorderRadius.circular(20)
+                      ),
+                    ),
+                    onChanged: (String value) {
+                      if (value.length == 1) {
+                        FocusScope.of(context).nextFocus();
+                      }
+                    },
+                  ),
+                ),
+                SizedBox(width: 30),
+                Expanded(
+                  child: TextField(
+                    controller: _pinController3,
+                    keyboardType: TextInputType.number,
+                    maxLength: 1,
+                    obscureText: false,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(10),
+
+                      counterText: '',
+                      border:OutlineInputBorder(borderRadius: BorderRadius.circular(20)
+                      ),
+                    ),
+                    onChanged: (String value) {
+                      if (value.length == 1) {
+                        FocusScope.of(context).nextFocus();
+                      }
+                    },
+                  ),
+                ),
+                SizedBox(width: 30),
+                Expanded(
+                  child: TextField(
+                    controller: _pinController4,
+                    keyboardType: TextInputType.number,
+                    maxLength: 1,
+                    obscureText: false,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(10),
+
+                      counterText: '',
+                      border:OutlineInputBorder(borderRadius: BorderRadius.circular(20)
+                      ),
+                    ),
+                    onChanged: (String value) {
+                      if (value.length == 1) {
+                        FocusScope.of(context).unfocus();
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 10),
+            Center(
+              child: RichText(
+                text: TextSpan(
+                  text: 'NOTE: ',
+                  style: TextStyle(color: Colors.blue),
+                  children: [
+                    TextSpan(
+                      text: 'This pin will be used in every transaction. Never share or show this to anybody.',
+                      style: TextStyle(color: Colors.black)
+                    )
+                  ]
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+
+                      ),
+                      backgroundColor: Colors.green
+                    ),
+                    onPressed: () {},
+                    child: Text('Finish',style: TextStyle(color: Colors.white),)
+                ),
+              ),
+            ),
+           ]))],
+        ),
       ),
     );
   }
 }
-
