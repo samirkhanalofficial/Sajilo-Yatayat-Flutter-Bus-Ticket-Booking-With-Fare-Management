eb714149059a51d0e703aedfe1619606ea65087a
samir khanal <52138139+samirkhanalofficial@users.noreply.github.com>
2024-02-25 09:48:17 +0545

    Revert "added owner registration and profie page"

--- lib/config/routes/routes_names.dart
+++ lib/config/routes/routes_names.dart
@@ -1,8 +1,4 @@
-
 class RoutesNames {
   static const homePage = '/';
   static const otpVerificationPage = '/otp-verification';
-  // ignore: constant_identifier_names
-  static const regpage = '/ownerreg';
-  static const ownerProfile='/owner_profile';
 }
