946806b3833f389c39ce97973694c00ef4e0c949
samirkhanalofficial <samirkhanalofficial@gmail.com>
2024-03-03 13:23:08 +0545

    fix

--- lib/ui/pages/bus/avilable_bus_page.dart
+++ lib/ui/pages/bus/avilable_bus_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-import 'package:tryapp/ui/widgets/global/bus/bus_details__card.dart';
+import 'package:tryapp/ui/widgets/global/bus/bus_details_card.dart';
 
 class AvilableBusPage extends StatelessWidget {
   const AvilableBusPage({super.key});
