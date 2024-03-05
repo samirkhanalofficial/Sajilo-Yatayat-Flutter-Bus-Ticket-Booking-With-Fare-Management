2c12b2bb4094cd51c01e8668cd93699384ff3cd3
sumit ray <sumitray206@gmail.com>
2024-03-05 10:11:06 +0545

    transication added

---
+++ lib/ui/pages/qrcode/generate_qr_code_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:tryapp/models/fare_details.dart';
+
+class GenerateQrPage extends StatelessWidget {
+  final String fareId;
+  const GenerateQrPage({super.key, required this.fareId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: [
+            const SizedBox(
+              height: 150,
+            ),
+            Center(
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: QrImageView(
+                  data: fareId,
+                  version: QrVersions.auto,
+                  size: 320,
+                  gapless: false,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Text(
+              "This QR contains your profile ID, which can be used to see your balance & make transaction with your Pin. Don’t share it to anybody, only show it to let them scan it.",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
