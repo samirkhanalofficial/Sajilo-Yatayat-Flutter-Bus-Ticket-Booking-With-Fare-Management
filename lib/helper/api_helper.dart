fa7f16eb89ca171cd4e7cef8bf9ac85941f2dfa4
samirkhanalofficial <samirkhanalofficial@gmail.com>
2024-03-08 23:55:15 +0545

    vcode 2(1.0.2)

--- lib/helper/api_helper.dart
+++ lib/helper/api_helper.dart
@@ -46,6 +46,7 @@ class APIHelper<T> extends GetxController {
           break;
       }
       var resBody = json.decode(res.body);
+      print(resBody);
       if (res.statusCode == successStatusCode) {
         successfullResponse(true);
         response.value = parseJsonToObject(resBody);
